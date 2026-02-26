ebe14962648d601c202d0a12ae86deb10c086cc0
hanseld28 <hansel.donizete@gmail.com>
2026-02-26 01:51:33 -0300

    fix: fixes base url

--- lib/utils/get-url.ts
+++ lib/utils/get-url.ts
@@ -3,20 +3,20 @@ const PRODUCTION_FALLBACK_URL = 'https://meuinfoid.com.br';
 
 export const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_SITE_URL) {
-    return process.env.NEXT_PUBLIC_SITE_URL;
+    return process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "");
   }
 
   if (typeof window !== 'undefined') {
     return window.location.origin;
   }
 
+  if (process.env.NODE_ENV === 'production') {
+    return PRODUCTION_FALLBACK_URL;
+  }
+
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  const defaultFallbackUrl = process.env.NODE_ENV === 'development'
-    ? LOCAL_DEVELOPMENT_FALLBACK_URL
-    : PRODUCTION_FALLBACK_URL;
-
-  return defaultFallbackUrl;
+  return LOCAL_DEVELOPMENT_FALLBACK_URL;
 };
